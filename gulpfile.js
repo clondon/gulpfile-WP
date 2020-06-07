35020d9cd7755146f478badaa3ee6ecbb7c77784
Charles <charles@new-web-sites.com>
2020-06-07 22:13:57 +0100

    Bare
    Bare gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require( 'gulp-dart-scss' );
+
+function scssCompile(cb) {
+gulp.task('sass', function () {
+    return gulp.src('./scss/**/*.scss')
+    .pipe(scss().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+})
+
+gulp.task( 'sass:watch', function () {
+    gulp.watch ( './sass/**/*.scss'['sass'] );
+})
+cb();
+}
+exports.default = scssCompile
+
